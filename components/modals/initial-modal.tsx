eac265f1ed4c3564925e1eb6726cc3851f18be54
ShiRin <2463951865z@gmail.com>
2025-07-05 16:13:26 +0800

    modal UI completed

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required.",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required.",
+  }),
+});
+
+const InitialModal = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <Dialog open>
+        <DialogContent className="overflow-hidden bg-white p-0 text-black">
+          <DialogHeader className="px-6 pt-8">
+            <DialogTitle className="text-center text-2xl font-bold">
+              Customize your server
+            </DialogTitle>
+            <DialogDescription className="text-center text-zinc-500">
+              Give your server a personality with a name and an image. You an
+              always change it later.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="space-y-8 px-6">
+                <div className="flex items-center justify-center">
+                  TODO: Image Upload
+                </div>
+                <FormField
+                  name="name"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="dark:text-secondary/70 text-xs font-bold !text-zinc-500 uppercase">
+                        Server name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className="border-0 !bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+                          placeholder="Enter server name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter className="bg-gray-100 px-6 py-4">
+                <Button variant="primary" disabled={isLoading}>
+                  Create
+                </Button>{" "}
+                {/*响应式的*/}
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+export default InitialModal;
