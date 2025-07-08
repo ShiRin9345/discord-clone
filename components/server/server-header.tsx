72913c0c531a0f24edd42d24060b56d61077be0d
ShiRin <2463951865z@gmail.com>
2025-07-08 18:35:11 +0800

    channel creation completed

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -67,7 +67,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm text-indigo-600 dark:text-indigo-400">
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel", { server })}
+            className="cursor-pointer px-3 py-2 text-sm text-indigo-600 dark:text-indigo-400"
+          >
             Create Channel
             <PlusCircle className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
