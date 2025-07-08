230ee8ed1d64e4a51d2ef39a648bcedabb06d2a9
ShiRin <2463951865z@gmail.com>
2025-07-08 17:57:20 +0800

    members management completed

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -58,7 +58,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm text-indigo-600 dark:text-indigo-400">
+          <DropdownMenuItem
+            onClick={() => onOpen("members", { server })}
+            className="cursor-pointer px-3 py-2 text-sm text-indigo-600 dark:text-indigo-400"
+          >
             Manage Members
             <Users className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
