b426b8f18a9ae29f1b2325296a34124dd4abe50e
ShiRin <2463951865z@gmail.com>
2025-07-10 18:50:09 +0800

    fix text bug of server-channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -21,7 +21,6 @@ const iconMap = {
 
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
   const params = useParams();
-  const router = useRouter();
 
   const Icon = iconMap[channel.type];
 
@@ -48,7 +47,7 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
             <ActionTooltip label="Edit">
               <Edit className="hidden h-4 w-4 text-zinc-500 transition group-hover:block hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300" />
             </ActionTooltip>
-            <ActionTooltip label="Edit">
+            <ActionTooltip label="Delete">
               <Trash className="hidden h-4 w-4 text-zinc-500 transition group-hover:block hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300" />
             </ActionTooltip>
           </div>
