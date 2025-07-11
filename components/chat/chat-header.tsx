97aee08445e4c3f3634386f11946ede0be1df552
ShiRin <2463951865z@gmail.com>
2025-07-11 11:26:15 +0800

    use flex-row to fix the layout error in mobile-toggle

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Hash, Menu } from "lucide-react";
+import { Hash } from "lucide-react";
+import MobileToggle from "@/components/mobile-toggle";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -11,7 +12,7 @@ interface ChatHeaderProps {
 const ChatHeader = ({ serverId, name, imageUrl, type }: ChatHeaderProps) => {
   return (
     <div className="text-md flex h-12 items-center border-b-2 border-neutral-200 px-3 font-semibold dark:border-neutral-800">
-      <Menu />
+      <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
       )}
