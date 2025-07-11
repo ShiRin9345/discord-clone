59eafbe29847f8e087bb0133fc057a4b2d05ad5c
ShiRin <2463951865z@gmail.com>
2025-07-11 11:05:29 +0800

    last fix is falst.Now, i correct the spelling error of 'hidden'

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="hideen fixed inset-y-0 z-30 h-full w-[72px] flex-col md:flex">
+      <div className="fixed inset-y-0 z-30 hidden h-full w-[72px] flex-col md:flex">
         <NavigationSidebar />
       </div>
       <main className="h-full md:pl-[72px]">{children}</main>
